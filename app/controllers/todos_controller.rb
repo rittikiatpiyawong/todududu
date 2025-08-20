93817b8b4a192219a900ff6e476c922b076c1496
rittiko <ritthikiat.piyawong@kkumail.com>
2025-08-20 10:22:46 +0700

    feat: implement todos controller to create todo and create turbo file

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,8 +8,26 @@ class TodosController < ApplicationController
   end
 
   def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.turbo_stream
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to root_path, alert: @todo.errors.full_messages.join(", ") }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("todo_form", partial: "todos/form", locals: { todo: @todo }) }
+      end
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :completed)
   end
 end
